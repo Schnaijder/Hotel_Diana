ceacdf21af9d278a0ccba24d16b16acd98990ed9
Mayte D. Melquiades <maydeni2910@gmail.com>
2014-12-08 02:19:36 -0600

    Agregué una pequeña reseña histórica de la ciudad en donde se encuentra ubicado el hotel, ya que la cliente lo pidió.

---
+++ resena.php
@@ -0,0 +1,221 @@
+
+
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Hotel Diana</title>
+ <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style_1.css" />
+</head>
+
+<body>
+   
+         
+	<div id="header">
+            <a  id="logo"><img src="logoHotel.png" alt="" width="207" height="88" /></a>																																																																																					
+		<span>Bienvenidos Al Hotel Diana</span>
+		<ul id="menu">
+                    <li><a href="Inicio.php">Inicio</a></li>
+                    <li><a href="Habitaciones.php">Habitaciones</a></li>
+			<li><a href="#">Servicios</a></li>
+                        <li><a href="Regla.php">Reglamento</a></li>
+			 <li><a href="resena.php">Reseña</a></li>
+                        <li><a href="MAPITA.php">Ubicacion</a></li>
+                        <li><a href="Contactanos.php">Contactanos</a></li>
+		</ul>
+             
+            <img src="p.jpg" alt="" width="800" height="300" /><br />
+             
+             <div id="copyright">																																																																																																											
+			</div>
+	</div>
+	<div id="content">
+		<div class="inside">
+			<div id="sidebar">
+				<div class="order">
+					<form action="#">
+						<h3></h3>
+						<table width="239" height="300" cellpadding="0" cellspacing="0">
+							
+                                                 
+									<tr>
+								<td class="first">
+		Calle.Jaimes pte. #45, Col.Centro
+Cd. Altamirano, Guerrero, 40730, Mexico
+hotel_Diana10@hotmail.es
++767 1045678 | 732 6740895 | 732 1048388<br />
+									
+								</td>
+                                                                
+                                                                </tr>
+                                                                
+                                                                	<tr>
+								<td class="first">
+									<br />
+									
+								</td>
+								
+							</tr>
+								
+							
+                                                          
+							   
+                                                 
+                                                    
+ 
+
+   
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    
+    <meta name="AUTHOR" content="SnackTools" />
+
+    <link rel="icon" type="image/x-icon" href="/favicon-bannersnack.ico" />
+
+                        <meta property="og:title" content="hotel diana" />
+                            <meta property="og:video" content="/app/swf2/EmbedPlayerV2.swf?hash_id=bxmetywlt&useHTTPS=true&clickTag=null&t=1416254294" />
+                            <meta property="og:video:height" content="250" />
+                            <meta property="og:video:width" content="250" />
+                            <meta property="og:video:type" content="application/x-shockwave-flash" />
+                            <meta property="og:image" content="http://files.bannersnack.net/thumbs/bxmetywlt" />
+            
+    
+
+
+<table id="itemContent">
+    <tr>
+        <td style="">
+            <div>
+                               
+                                <iframe src="http://files.bannersnack.com/iframe/embed.html?hash=bxmetywlt&wmode=transparent&flashvars=direct_link%3Dtrue&t=1416254294" width="250" height="250" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
+                
+            </div>
+        </td>
+    </tr>
+</table>                               
+                                
+                
+                                                    
+                                       
+                                                    
+                                                    
+                                                    
+                                                    
+						</table>
+																																																																																																																	
+					</form>
+				</div>
+                            
+                            
+                                                
+ 
+
+
+				<div class="block">
+                                    <center><h4>Ubicacion Geografica</h4></center>
+					<p>Nuestro hotel esta ubicado en nuestra bella ciudad de altamirano guerrero , entre las calles san pedro y san antonio, colonia centro.</p>
+                                        <img src="ma.png" alt="" width="304" height="207" class="map" /><br />
+					<a  class="more">Mas info...</a> 
+				</div>
+                            
+                            
+                            <center><span>Redes Sociales:</span></center>
+<center>     <a href="https://www.facebook.com/pages/Hotel-Diana/440762639301365?ref=ts&fref=ts" ><img alt="Visitanos" height="50" src="Facebook.png" title="Visitanos" width="50" /></a>
+                                        <a href="" ><img alt="Visitanos" height="50" src="twitter.png" title="Visitanos" width="50" /></a>
+                                        <a href="" ><img alt="Visitanos" height="50" src="instagram.png" title="Visitanos" width="50" /></a>
+                                        <a href="https://www.youtube.com/watch?v=xaxcNchG5Ok" ><img alt="Visitanos" height="50" src="youtube.png" title="Visitanos" width="50" /></a>
+            
+</center>
+			</div>
+                            
+                    <center><p id="t"><b>Ciudad Altamirano<b/></p>   </center>
+                    <br/><br/>
+                    <figure>
+                        <center>
+                        <img  style="height: 314px; width: 310px; " src="reseña/Cd.Altamirano-Catedral.JPG"/>
+                        </center>
+                    </figure>
+  <p align="justify"><b>Ciudad Altamirano</b>, más conocida sólo como <b>Altamirano</b>, es una ciudad 
+        mexicana del estado de Guerrero, en la región de Tierra Caliente. Es cabecera del municipio de Pungarabato.</p>
+        <br/>
+        <br/>
+        <p align="justify">Se ubica junto al río Cutzamala, una de las principales afluentes del río Balsas, 
+        la cual sirve en esta región de frontera entre los estados de Guerrero y 
+        Michoacán, situándose esta ciudad en la porción fronteriza de los dos. Ciudad Altamirano 
+        funciona como entronque de las carreteras federales 134 y 
+        51, que comunican en el estado a Cutzamala de Pinzón con Zihuatanejo e 
+        Iguala con Ciudad Altamirano, respectivamente.</p>
+        <br/>
+        <br/>
+            <figure>
+                <center>
+                    <img  style="height: 314px; width: 231px; " src="reseña/Escudo_de_Ciudad_Altamirano.png"/>
+                </center>
+            </figure>
+       <p align="justify">El escudo de Ciudad Altamirano lo integran las figuras de la iglesia, el puente sobre el río 
+            Cutzamala, el cerro de Chuperio, un tractor, una milpa y un zopilote, que simbolizan la unidad, 
+            el trabajo y el progreso. Se describe así: “Conserva un formato tradicional en forma de ‘U’, 
+            rematada con un penacho en la parte superior.</p>
+        <br/><br/>
+<p align="justify">En su interior destaca la regia figura del cerro Chuperio, mudo testigo de tantos hechos que han 
+    transcurrido a lo largo de toda la historia, desde la fundación del antiguo Pungarabato 
+    (la Conquista, la Independencia, la Revolución, etc.), hasta nuestros días. En el cielo aparece 
+    la apacible silueta de un zopilote, ave de rapiña común en esta región, que, lamentablemente, tiende a 
+    desaparecer–. En el centro sobresale la fisonomía de la catedral, original monumento arquitectónico, 
+    construido en diferentes épocas, que engloba la ciudad, la familia, su personalidad, la sociedad, la 
+    población y su idiosincrasia. En el costado izquierdo se ve la ‘Cruz de Mayo’, símbolo de la fe y la 
+    leyenda, donde se dice que está hincado el ‘Báculo Sagrado’ del ‘Padre Santo’, Fray Juan Bautista Moya, 
+    fundador de esta ciudad, como testimonio de paz y tranquilidad.</p>
+<br/><br/>
+<p align="justify">En el flanco derecho se aprecia un sembradío de maíz, uno de los principales cultivos de este 
+    municipio, mismo que representa el desarrollo del campo. El tractor, la expresión del avance 
+    tecnológico. Todo esto, moviéndose en torno al único creador de sus costumbres y tradiciones, 
+    testigo de su presente y su pasado: el hombre. En la parte inferior se ve la corriente del río 
+    Cutzamala, que baña la ribera norte de la ciudad, y mitiga el anhelo de esa zona, representando 
+    magia, vida y canto, embellecido por el puente, lazo indispensable entre el comercio, el progreso y la comunicación.</p>
+<br/><br/>
+<p align="justify">En la parte superior, en vez del tradicional penacho de plumas, queda plasmado el engrane, que 
+    constituye el trabajo, la industria y la productividad. Como texto se lee, en primer término y 
+    en forma horizontal, el nombre de CIUDAD ALTAMIRANO, GUERRERO, cabecera del municipio de Pungarabato 
+    y la ciudad de mayor importancia de esta vasta región de la Tierra Caliente, de los estados de México, 
+    Michoacán y Guerrero. Siguiendo el contorno del escudo, y leyendo de izquierda a derecha y de arriba 
+    hacia abajo, a manera de lema, se presentan las palabras: TRABAJO, PROGRESO Y UNIDAD, filosofía del 
+    altamiranense de todos los tiempos. Respecto a los colores, el escudo está enmarcado con un color oro, 
+    por ser éste el elemento básico de una de las más importantes industrias de Ciudad Altamirano, la orfebrería, 
+    famosa por sus maravillosas filigranas. El conjunto cromático interno es afín a la naturaleza.</p>
+<br/><br/>
+<p align="justify">Esta descripción podría resumirse de la siguiente manera: Se tiene en un solo emblema la representación 
+    gráfica del Pungarabato antiguo y moderno, lo que hoy es Ciudad Altamirano, Guerrero. Su historia: el 
+    Cerro Chuperio; su leyenda: la Cruz de Mayo; la industria: el engrane; el comercio y las comunicaciones: 
+    el puente; el campo: el maíz; la ciudad: la Catedral; la vida: el río y el cielo; su mentalidad: el lema, 
+    y, lo más importante, el elemento humano: el campesino. Este emblema fue creado por el licenciado Arturo 
+    Villela Hernández y aprobado en sesión de cabildo el día 3 de mayo de 1983, siendo presidente municipal el 
+    señor Tirso Castillo Molina”.</p>
+<br/><br/>
+        
+			</div>
+		</div>	
+	</div>
+    
+	<div id="footer">
+		<ul>
+                    <li><a href="Inicio.php">Inicio</a></li>
+                    <li><a href="Habitaciones.php">Habitaciones</a></li>
+			<li><a href="#">Servicios</a></li>
+                        <li><a href="Regla.php">Reglamento</a></li>
+                        <li><a href="resena.php">Reseña</a></li>
+                        <li><a href="MAPITA.php">Ubicacion</a></li>
+                        <li><a href="Contactanos.php">Contactanos</a></li>
+		</ul>
+		<div id="copyright">																																																																																																											
+			 <span><a>©Copyright. Todos los derechos reservados</a></span>
+			<span><a  target="_blank">Hotel Diana Cd.Altamirano.Guerrero</a></span>
+                        <li><a href="politicas.php">Politicas de Reservacion</a></li>																																																																	
+		</div>
+	</div>
+    </body>
+    
+</html>
